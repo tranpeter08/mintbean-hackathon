3ef6329d8a4ee53203da2e05e70c57ca5f120ba8
Peter <tran.peter08@gmail.com>
2021-11-23 14:09:26 -0800

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,19 @@
-import {Box} from '@chakra-ui/react';
+import {Box, Text, Link} from '@chakra-ui/react';
 
 export default function Footer(): JSX.Element {
   return (
     <footer className="footer">
-      <Box mt={10}>
-        Created by <a href="https://github.com/tranpeter08">Peter Tran</a>
+      <Box backgroundColor="black" width="100%" mt={10}>
+        <Text color="white" p={5} textAlign="center">
+          This web application was created by{' '}
+          <Link
+            color="blue.500"
+            fontWeight="bold"
+            href="https://github.com/tranpeter08"
+          >
+            Peter Tran
+          </Link>
+        </Text>
       </Box>
     </footer>
   );
