e2163e8de48f575fc9cd0159ad1d5300a237e2b9
Peter <tran.peter08@gmail.com>
2021-11-23 14:10:36 -0800

    siteName is dynamic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Layout from '../components/Layout';
 import Head from 'next/head';
+import {settings} from '../config/settings';
 
 function MyApp({Component, pageProps}: AppProps) {
   return (
     <>
       <Head>
-        <title>MintBeam Hackathon</title>
+        <meta
+          name="description"
+          content="Doodle your great masterpiece and share with others!"
+        />
+        <title>{settings.siteName}</title>
       </Head>
       <Layout>
         <main className="content">
