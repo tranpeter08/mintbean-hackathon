62fe39b1aa7e093158127fe10e5f8fdcb359bc85
Peter <tran.peter08@gmail.com>
2021-11-24 03:34:55 -0800

    updated page routes

--- src/config/pageRoutes.ts
+++ src/config/pageRoutes.ts
@@ -1,19 +1,27 @@
-interface PageData {
-  href: string;
-  name: string;
-}
+// interface PageData {
+//   href: string;
+//   name: string;
+// }
 
-interface PageRoutes {
-  [key: string]: PageData;
-}
+// interface PageRoutes {
+//   [key: string]: PageData;
+// }
 
-export const pageRoutes: PageRoutes = {
+export const pageRoutes = {
   home: {
     href: '/',
     name: 'Home',
   },
+  gallery: {
+    href: '/gallery',
+    name: 'Gallery',
+  },
   draw: {
     href: '/draw',
     name: 'Draw',
+  },
+  signup: {
+    href: '/signup',
+    name: 'Signup',
   },
 };
