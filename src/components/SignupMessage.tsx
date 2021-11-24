98379947407c957caf669d2e1c8b4641f0f91f32
Peter <tran.peter08@gmail.com>
2021-11-24 03:33:08 -0800

    created component for signup message

---
+++ src/components/SignupMessage.tsx
@@ -0,0 +1,18 @@
+import {Text, Link, Box, Flex} from '@chakra-ui/react';
+import NextLink from 'next/link';
+import {pageRoutes} from '../config/pageRoutes';
+
+export default function SignUpMessage(props) {
+  return (
+    <Flex justifyContent="center">
+      <Text fontSize={24}>
+        Looks like the art gallery is empty. Be the first to share your art
+        work,{' '}
+        <NextLink href={pageRoutes.signup.href} passHref>
+          <Link color="blue.500">signup</Link>
+        </NextLink>{' '}
+        today!
+      </Text>
+    </Flex>
+  );
+}
