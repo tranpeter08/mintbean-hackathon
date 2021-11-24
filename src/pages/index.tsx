6fd98d273146e7660f9b78c3dc156f68f4bb4cf6
Peter <tran.peter08@gmail.com>
2021-11-24 03:36:45 -0800

    refactor home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ import {
   useRef,
 } from 'react';
 import axios from 'axios';
-import {Grid, Heading} from '@chakra-ui/react';
+import {Grid, Heading, Box, Text} from '@chakra-ui/react';
 import {connectToDatabase} from '../database/connect';
 import {DrawingData} from '../types';
 import DrawingCard from '../components/DrawingCard';
@@ -25,62 +25,35 @@ interface HomeProps {
 }
 
 const Home = (props: HomeProps) => {
-  const {drawings} = props;
-
   return (
     <>
       <Head>
         <title>Drawing Gallery - {settings.siteName}</title>
       </Head>
-      <Heading textAlign="center" as="h1">
+
+      {/* Hero */}
+      <Grid>
+        <Grid></Grid>
+        <Grid></Grid>
+      </Grid>
+      <Grid></Grid>
+
+      {/* Tech Stack */}
+      <Heading></Heading>
+      <Text></Text>
+      <Grid>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+        <Box></Box>
+      </Grid>
+
+      {/* About Us */}
+      <Heading mt={10} textAlign="center" as="h1">
         Recent Drawings
       </Heading>
-      <Grid
-        p={6}
-        templateColumns={{
-          base: 'repeat(3, 1fr)',
-          md: 'repeat(4, 1fr)',
-          lg: 'repeat(5, 1fr)',
-        }}
-        gap={6}
-      >
-        {drawings &&
-          drawings.map((drawing, index) => {
-            return (
-              <DrawingCard
-                key={drawing._id}
-                src={drawing.url}
-                alt={`drawing #${index + 1}`}
-                id={drawing._id}
-              />
-            );
-          })}
-      </Grid>
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  try {
-    const {db} = await connectToDatabase();
-    const results = await db
-      .collection('drawings')
-      .find({}, {limit: 100})
-      .project({_id: true, url: true})
-      .toArray();
-
-    const drawings = JSON.parse(JSON.stringify(results));
-
-    return {
-      props: {drawings},
-      revalidate: 30,
-    };
-  } catch (error) {
-    console.log(error);
-    return {
-      props: {drawings: []},
-    };
-  }
 };
 
 export default Home;
