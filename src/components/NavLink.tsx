35eec27d30a6e33a8afefd4458beafb52ba30999
Peter <tran.peter08@gmail.com>
2021-11-24 03:31:49 -0800

    added event listener for responsive menu

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,23 +1,31 @@
 import NextLink from 'next/link';
 import {Link, ListItem, Box} from '@chakra-ui/react';
 import {useRouter} from 'next/router';
+import {MouseEventHandler} from 'react';
 
 interface NavLinkProps {
   href: string;
   name: string;
   className?: string;
+  onClick?: MouseEventHandler;
 }
 
-export default function NavLink({href, name, className}: NavLinkProps) {
+export default function NavLink({
+  href,
+  name,
+  className,
+  onClick,
+}: NavLinkProps) {
   let anchorClassName = typeof className === 'string' ? className : '';
   const router = useRouter();
   const {pathname} = router;
   const isActive = pathname === href;
 
   return (
-    <Box pl={5}>
+    <Box p={3}>
       <NextLink href={href} passHref>
         <Link
+          onClick={onClick}
           fontSize={18}
           fontWeight={isActive ? 700 : 500}
           color={isActive ? 'blue.700' : 'black'}
