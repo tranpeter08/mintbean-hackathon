fee9b5484a8a50564086dfa6ed1b51bbdc4d8f86
Peter <tran.peter08@gmail.com>
2021-11-24 03:34:01 -0800

    updated styling

--- src/components/DrawingCard.tsx
+++ src/components/DrawingCard.tsx
@@ -1,4 +1,4 @@
-import {Box, Image, Link} from '@chakra-ui/react';
+import {Image, Link, Flex} from '@chakra-ui/react';
 import NextLink from 'next/link';
 
 interface DrawingCardProps {
@@ -10,22 +10,10 @@ interface DrawingCardProps {
 export default function DrawingCard(props: DrawingCardProps) {
   const {src, id, alt} = props;
 
-  const responsive = {
-    base: 100,
-    md: 150,
-    lg: 180,
-  };
-
   return (
     <NextLink href={`/drawing/${id}`} passHref>
-      <Link>
-        <Image
-          shadow="xl"
-          _hover={{borderColor: 'teal.500', borderWidth: 1}}
-          boxSize={responsive}
-          src={src}
-          alt={alt}
-        />
+      <Link backgroundColor="white" shadow="2xl" _hover={{shadow: 'dark-lg'}}>
+        <Image src={src} alt={alt} />
       </Link>
     </NextLink>
   );
