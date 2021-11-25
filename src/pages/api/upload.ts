96f10f96f6886dc5510b00a018518ad4da69bb3f
Peter Tran <tran.peter08@gmail.com>
2021-11-24 16:12:14 -0800

    refactor

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,9 +1,11 @@
-import type {NextApiRequest, NextApiResponse} from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
 import Cloudinary from 'cloudinary';
 import formidable from 'formidable';
-import {connectToDatabase} from '../../database/connect';
+import { connectToDatabase } from '../../database/connect';
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
+import DrawingService from '../../api-lib/services/DrawingService';
 
-export default async function upload(
+export default withApiAuthRequired(async function upload(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
@@ -11,29 +13,37 @@ export default async function upload(
     if (req.method !== 'POST') throw 'Invalid HTTP request method';
 
     const cloudinary = Cloudinary.v2;
-
     const data: any = await new Promise(function (resolve, reject) {
-      const form = new formidable.IncomingForm({keepExtensions: true});
+      const form = new formidable.IncomingForm({ keepExtensions: true });
       form.parse(req, function (err, fields, files) {
         if (err) return reject(err);
-        resolve({fields, files});
+        resolve({ fields, files });
       });
     });
 
-    const result: any = await cloudinary.uploader.upload(data.fields.file);
-    const {url} = result;
+    const { penData, file } = data?.fields;
 
-    const {db} = await connectToDatabase();
-    const dbData = await db
-      .collection('drawings')
-      .insertOne({url, penData: data.fields.penData});
+    if (typeof penData === 'undefined' || typeof file === 'undefined')
+      throw new Error('Missing required fields');
 
-    res.status(201).send({success: dbData.acknowledged});
+    const result: any = await cloudinary.uploader.upload(file);
+    const { url } = result;
+    const { db } = await connectToDatabase();
+    const drawingService = new DrawingService(db);
+    const { user } = getSession(req, res);
+
+    const isSucces = drawingService.createDrawing({
+      email: user.email,
+      penData: data.fields.penData,
+      url,
+    });
+
+    res.status(201).send({ success: isSucces });
   } catch (error) {
     console.log(error);
-    res.status(400).send({message: 'Bad Request'});
+    res.status(400).send({ message: 'Bad Request' });
   }
-}
+});
 
 export const config = {
   api: {
