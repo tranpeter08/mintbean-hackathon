27bdc636c592d3bddc0bcb899c5e66148c4fda4a
Peter Tran <tran.peter08@gmail.com>
2021-11-24 16:09:37 -0800

    added auth0

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,27 @@
 import '../styles/globals.css';
-import type {AppProps} from 'next/app';
+import type { AppProps } from 'next/app';
 import Layout from '../components/Layout';
 import Head from 'next/head';
-import {settings} from '../config/settings';
+import { settings } from '../config/settings';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-function MyApp({Component, pageProps}: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta
-          name="description"
-          content="Doodle your great masterpiece and share with others!"
+          name='description'
+          content='Doodle your great masterpiece and share with others!'
         />
         <title>{settings.siteName}</title>
       </Head>
-      <Layout>
-        <main className="content">
-          <Component {...pageProps} />
-        </main>
-      </Layout>
+      <UserProvider>
+        <Layout>
+          <main className='content'>
+            <Component {...pageProps} />
+          </main>
+        </Layout>
+      </UserProvider>
     </>
   );
 }
