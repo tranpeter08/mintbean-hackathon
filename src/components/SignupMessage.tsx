df8b1e9bdaf76089a9116e71fc50156904bf51b3
Peter Tran <tran.peter08@gmail.com>
2021-11-24 16:56:31 -0800

    fixed typo

--- src/components/SignupMessage.tsx
+++ src/components/SignupMessage.tsx
@@ -1,15 +1,15 @@
-import {Text, Link, Box, Flex} from '@chakra-ui/react';
+import { Text, Link, Box, Flex } from '@chakra-ui/react';
 import NextLink from 'next/link';
-import {pageRoutes} from '../config/pageRoutes';
+import { pageRoutes } from '../config/pageRoutes';
 
 export default function SignUpMessage(props) {
   return (
-    <Flex justifyContent="center">
+    <Flex justifyContent='center'>
       <Text fontSize={24}>
         Looks like the art gallery is empty. Be the first to share your art
         work,{' '}
-        <NextLink href={pageRoutes.signup.href} passHref>
-          <Link color="blue.500">signup</Link>
+        <NextLink href={'/api/auth/login'} passHref>
+          <Link color='blue.500'>signup</Link>
         </NextLink>{' '}
         today!
       </Text>
